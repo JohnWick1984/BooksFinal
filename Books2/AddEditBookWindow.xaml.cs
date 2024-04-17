8de66b2793f18b17c52dd358061cb101b9049065
Евгений Шумаков <daredevil1984@list.ru>
2024-04-17 20:44:54 +0500

    Добавьте файлы проекта.

---
+++ Books2/AddEditBookWindow.xaml.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using static Books2.MainWindow;
+
+namespace Books2
+{
+    public partial class AddEditBookWindow : Window
+    {
+        private string connectionString = "Data Source=EUGENE1984; Initial Catalog=Books2; Integrated Security=True;";
+        private Book book;
+
+        public AddEditBookWindow()
+        {
+            InitializeComponent();
+            InitializeComboBox();
+        }
+
+        public AddEditBookWindow(Book book)
+        {
+            InitializeComponent();
+            InitializeComboBox();
+
+            this.book = book;
+
+            titleTextBox.Text = book.BookTitle;
+            pagesReadTextBox.Text = book.PagesRead.ToString();
+            totalPagesTextBox.Text = book.TotalPages.ToString();
+            ratingTextBox.Text = book.Rating?.ToString();
+            statusComboBox.SelectedItem = book.Status.StatusName;
+            authorFirstNameTextBox.Text = book.Author.FirstName;
+            authorLastNameTextBox.Text = book.Author.LastName;
+        }
+
+        private void InitializeComboBox()
+        {
+            statusComboBox.ItemsSource = new string[] { "Planning", "Reading", "Read", "Discontinued" };
+        }
+        private byte[] imageDataForNewBook;
+        private void SelectImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.jpg; *.jpeg; *.png; *.gif; *.bmp)|*.jpg; *.jpeg; *.png; *.gif; *.bmp|All Files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+               
+                string imagePath = openFileDialog.FileName;
+
+                
+                coverImage.Source = new BitmapImage(new Uri(imagePath));
+
+               
+                byte[] imageData = File.ReadAllBytes(imagePath);
+
+               
+                imageDataForNewBook = imageData;
+            }
+        }
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            string bookTitle = titleTextBox.Text;
+            Book existingBook = GetBookByTitle(bookTitle);
+
+            if (existingBook != null)
+            {
+                existingBook.AuthorId = GetOrCreateAuthorId(authorFirstNameTextBox.Text, authorLastNameTextBox.Text);
+                existingBook.PagesRead = int.Parse(pagesReadTextBox.Text);
+                existingBook.TotalPages = int.Parse(totalPagesTextBox.Text);
+                existingBook.StatusId = GetStatusId(statusComboBox.SelectedItem.ToString());
+                existingBook.Rating = string.IsNullOrEmpty(ratingTextBox.Text) ? (int?)null : int.Parse(ratingTextBox.Text);
+                UpdateBookInDatabase(existingBook);
+                MessageBox.Show("Данные книги успешно обновлены!");
+            }
+            else
+            {
+                Book newBook = new Book
+                {
+                    BookTitle = bookTitle,
+                    Author = new Author
+                    {
+                        FirstName = authorFirstNameTextBox.Text,
+                        LastName = authorLastNameTextBox.Text
+                    },
+                    PagesRead = int.Parse(pagesReadTextBox.Text),
+                    TotalPages = int.Parse(totalPagesTextBox.Text),
+                    StatusId = GetStatusId(statusComboBox.SelectedItem.ToString()),
+                    Rating = string.IsNullOrEmpty(ratingTextBox.Text) ? (int?)null : int.Parse(ratingTextBox.Text)
+                };
+                AddBookToDatabase( newBook, imageDataForNewBook);
+                MessageBox.Show("Новая книга успешно добавлена!");
+            }
+
+            Close();
+            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            if (mainWindow != null)
+            {
+                Task.Run(() => mainWindow.LoadBooksAsync());
+            }
+        }
+
+        private Book GetBookByTitle(string title)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM Books WHERE Book_Title = @Title", connection);
+                command.Parameters.AddWithValue("@Title", title);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    return new Book
+                    {
+                        BookId = (int)reader["Book_ID"],
+                        BookTitle = (string)reader["Book_Title"],
+                        AuthorId = (int)reader["Author_ID"],
+                        PagesRead = (int)reader["Pages_Read"],
+                        TotalPages = (int)reader["Total_Pages"],
+                        StatusId = (int)reader["Status_ID"],
+                        Rating = reader["Rating"] != DBNull.Value ? (int?)reader["Rating"] : null
+                    };
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        private int GetOrCreateAuthorId(string firstName, string lastName)
+        {
+            int authorId = GetAuthorId(firstName, lastName);
+            if (authorId == -1)
+            {
+                authorId = CreateAuthor(firstName, lastName);
+            }
+            return authorId;
+        }
+
+        private int GetAuthorId(string firstName, string lastName)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT Author_ID FROM Authors WHERE First_Name = @FirstName AND Last_Name = @LastName", connection);
+                command.Parameters.AddWithValue("@FirstName", firstName);
+                command.Parameters.AddWithValue("@LastName", lastName);
+                object result = command.ExecuteScalar();
+                return result != null ? (int)result : -1;
+            }
+        }
+
+        private int CreateAuthor(string firstName, string lastName)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("INSERT INTO Authors (First_Name, Last_Name) VALUES (@FirstName, @LastName); SELECT SCOPE_IDENTITY();", connection);
+                    command.Parameters.AddWithValue("@FirstName", firstName);
+                    command.Parameters.AddWithValue("@LastName", lastName);
+                    return Convert.ToInt32(command.ExecuteScalar());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка при добавлении нового автора в базу данных: " + ex.Message);
+                return -1;
+            }
+        }
+
+        private int GetStatusId(string statusName)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT Status_ID FROM Statuses WHERE Status_Name = @StatusName", connection);
+                command.Parameters.AddWithValue("@StatusName", statusName);
+                object result = command.ExecuteScalar();
+                return result != null ? (int)result : -1;
+            }
+        }
+        private readonly object _lock = new object();
+        private void AddBookToDatabase(Book newBook, byte[] imageData)
+        {
+            lock (_lock)
+            {
+                try
+                {
+                   
+                    int authorId = GetAuthorId(newBook.Author.FirstName, newBook.Author.LastName);
+                    if (authorId == -1)
+                    {
+                      
+                        authorId = AddAuthorToDatabase(newBook.Author);
+                        if (authorId == -1)
+                        {
+                           
+                            MessageBox.Show("Не удалось добавить нового автора.");
+                            return;
+                        }
+                    }
+
+                   
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        SqlCommand command = new SqlCommand("INSERT INTO Books (Book_Title, Author_ID, Pages_Read, Total_Pages, Status_ID, Rating, Cover_Image) VALUES (@Title, @AuthorId, @PagesRead, @TotalPages, @StatusId, @Rating, @CoverImage)", connection);
+                        command.Parameters.AddWithValue("@Title", newBook.BookTitle);
+                        command.Parameters.AddWithValue("@AuthorId", authorId);
+                        command.Parameters.AddWithValue("@PagesRead", newBook.PagesRead);
+                        command.Parameters.AddWithValue("@TotalPages", newBook.TotalPages);
+                        command.Parameters.AddWithValue("@StatusId", newBook.StatusId);
+                        command.Parameters.AddWithValue("@Rating", newBook.Rating ?? (object)DBNull.Value);
+                        command.Parameters.AddWithValue("@CoverImage", imageData);
+                        command.ExecuteNonQuery();
+                    }
+
+                    MessageBox.Show("Новая книга успешно добавлена!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Произошла ошибка при добавлении новой книги в базу данных: " + ex.Message);
+                }
+            }
+        }
+        private int AddAuthorToDatabase(Author author)
+        {
+            try
+            {
+               
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("INSERT INTO Authors (First_Name, Last_Name) VALUES (@FirstName, @LastName); SELECT SCOPE_IDENTITY();", connection);
+                    command.Parameters.AddWithValue("@FirstName", author.FirstName);
+                    command.Parameters.AddWithValue("@LastName", author.LastName);
+                    return Convert.ToInt32(command.ExecuteScalar());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка при добавлении нового автора в базу данных: " + ex.Message);
+                return -1;
+            }
+        }
+
+        private void UpdateBookInDatabase(Book existingBook)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("UPDATE Books SET Book_Title = @Title, Author_ID = @AuthorId, Pages_Read = @PagesRead, Total_Pages = @TotalPages, Status_ID = @StatusId, Rating = @Rating WHERE Book_ID = @BookId", connection);
+                command.Parameters.AddWithValue("@Title", existingBook.BookTitle);
+                command.Parameters.AddWithValue("@AuthorId", existingBook.AuthorId);
+                command.Parameters.AddWithValue("@PagesRead", existingBook.PagesRead);
+                command.Parameters.AddWithValue("@TotalPages", existingBook.TotalPages);
+                command.Parameters.AddWithValue("@StatusId", existingBook.StatusId);
+                command.Parameters.AddWithValue("@Rating", existingBook.Rating ?? (object)DBNull.Value);
+                command.Parameters.AddWithValue("@BookId", existingBook.BookId);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ Books2/MainWindow.xaml.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Books2
+{
+    public partial class MainWindow : Window
+    {
+        private string connectionString = "Data Source=EUGENE1984; Initial Catalog=Books2; Integrated Security=True;";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Task.Run(() => LoadBooksAsync());
+        }
+
+        public async Task LoadBooksAsync()
+        {
+            List<Book> books = await Task.Run(() => LoadBooksFromDatabase());
+            Dispatcher.Invoke(() =>
+            {
+                bookComboBox.ItemsSource = books;
+                bookComboBox.DisplayMemberPath = "BookTitle";
+            });
+        }
+
+        private List<Book> LoadBooksFromDatabase()
+        {
+            List<Book> books = new List<Book>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT B.Book_ID, B.Book_Title, B.Pages_Read, B.Total_Pages, B.Rating, B.Cover_Image, " +
+                                                     "A.Author_ID, A.First_Name, A.Last_Name, " +
+                                                     "S.Status_ID, S.Status_Name " +
+                                                     "FROM [Books2].[dbo].[Books] AS B " +
+                                                     "INNER JOIN [Books2].[dbo].[Authors] AS A ON B.Author_ID = A.Author_ID " +
+                                                     "INNER JOIN [Books2].[dbo].[Statuses] AS S ON B.Status_ID = S.Status_ID", connection);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Book book = new Book
+                    {
+                        BookId = (int)reader["Book_ID"],
+                        BookTitle = (string)reader["Book_Title"],
+                        PagesRead = (int)reader["Pages_Read"],
+                        TotalPages = (int)reader["Total_Pages"],
+                        Rating = reader["Rating"] != DBNull.Value ? (int?)reader["Rating"] : null,
+                        Author = new Author
+                        {
+                            AuthorId = (int)reader["Author_ID"],
+                            FirstName = (string)reader["First_Name"],
+                            LastName = (string)reader["Last_Name"]
+                        },
+                        Status = new Status
+                        {
+                            StatusId = (int)reader["Status_ID"],
+                            StatusName = (string)reader["Status_Name"]
+                        }
+                    };
+
+                    if (reader["Cover_Image"] != DBNull.Value)
+                    {
+                        byte[] imageBytes = (byte[])reader["Cover_Image"];
+                        book.CoverImage = GetImageFromByteArray(imageBytes);
+                    }
+                    else
+                    {
+                        book.CoverImage = null;
+                    }
+
+                    books.Add(book);
+                }
+            }
+            return books;
+        }
+
+        private ImageSource GetImageFromByteArray(byte[] byteArray)
+        {
+            using (MemoryStream stream = new MemoryStream(byteArray))
+            {
+                BitmapImage image = new BitmapImage();
+                image.BeginInit();
+                image.StreamSource = stream;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.EndInit();
+                return image;
+            }
+        }
+
+        private async void bookComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            await UpdateBookInformationAsync();
+        }
+
+        private async Task UpdateBookInformationAsync()
+        {
+            if (bookComboBox.SelectedItem != null)
+            {
+                Book selectedBook = (Book)bookComboBox.SelectedItem;
+                authorTextBlock.Text = $"Автор: {selectedBook.Author.FirstName} {selectedBook.Author.LastName}";
+                titleTextBlock.Text = $"Название: {selectedBook.BookTitle}";
+                pagesReadTextBlock.Text = $"Прочитано страниц: {selectedBook.PagesRead}";
+                totalPagesTextBlock.Text = $"Всего страниц: {selectedBook.TotalPages}";
+                statusTextBlock.Text = $"Статус: {selectedBook.Status.StatusName}";
+                ratingTextBlock.Text = selectedBook.Rating != null ? $"Оценка: {selectedBook.Rating}" : "Оценка: не установлена";
+
+                await Task.Run(() =>
+                {
+                    BitmapImage bitmapImage = selectedBook.CoverImage as BitmapImage;
+                    if (bitmapImage != null)
+                    {
+                        using (MemoryStream stream = new MemoryStream())
+                        {
+                            BitmapEncoder encoder = new PngBitmapEncoder();
+                            encoder.Frames.Add(BitmapFrame.Create(bitmapImage));
+                            encoder.Save(stream);
+                            stream.Flush();
+                            stream.Position = 0;
+
+                            Dispatcher.Invoke(() =>
+                            {
+                               
+                                BitmapImage imageFromStream = new BitmapImage();
+                                imageFromStream.BeginInit();
+                                imageFromStream.CacheOption = BitmapCacheOption.OnLoad;
+                                imageFromStream.StreamSource = stream;
+                                imageFromStream.EndInit();
+
+                               
+                                coverImage.Source = imageFromStream;
+                            });
+                        }
+                    }
+                });
+            }
+        }
+
+        private void AddBook_Click(object sender, RoutedEventArgs e)
+        {
+            AddEditBookWindow addWindow = new AddEditBookWindow();
+            addWindow.ShowDialog();
+        }
+
+        private void EditBook_Click(object sender, RoutedEventArgs e)
+        {
+            Book selectedBook = (Book)bookComboBox.SelectedItem;
+            if (selectedBook != null)
+            {
+                AddEditBookWindow editWindow = new AddEditBookWindow(selectedBook);
+                editWindow.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Выберите книгу для редактирования.");
+            }
+        }
+
+        public class Book
+        {
+            [Key]
+            public int BookId { get; set; }
+            public string BookTitle { get; set; }
+            public int AuthorId { get; set; }
+            public int PagesRead { get; set; }
+            public int TotalPages { get; set; }
+            public int StatusId { get; set; }
+            public int? Rating { get; set; }
+            public ImageSource CoverImage { get; set; }
+            [ForeignKey("AuthorId")]
+            public Author Author { get; set; }
+            [ForeignKey("StatusId")]
+            public Status Status { get; set; }
+        }
+        public class Author
+        {
+            [Key]
+            public int AuthorId { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public ICollection<Book> Books { get; set; }
+        }
+        public class Status
+        {
+            [Key]
+            public int StatusId { get; set; }
+            public string StatusName { get; set; }
+            public ICollection<Book> Books { get; set; }
+        }
+    }
+}
